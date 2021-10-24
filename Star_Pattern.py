59d9b089db6d363596ddd917f288117d51bbb987
Pradyumna Kulkarni <92165960+pradyumnakulkarni87@users.noreply.github.com>
2021-10-24 13:54:45 +0530

    Star_patter Coding

---
+++ Star_Pattern.py
@@ -0,0 +1,79 @@
+'''
+for i in range(1,6):# 1,2,3,4,5
+    # i=1
+    for j in range(1,6):# 1,2,3,4,5
+        #j = 1
+        print(i,j,end='\t')
+-----------------------------------
+for i in range(1,6):# 1,2,3,4,5
+   print(chr(64+i))
+         
+-----------------------------------
+for i in range(1,6):# 1,2,3,4,5
+    for j in range(1,6):
+        print(chr(64+i),chr(64+j),end='\t')
+    print()
+--------------------------------------4
+
+for i in range(5):
+    print(chr(65+i))
+------------------------
+'''
+for i in range(1,10):#i = 3
+    for j in range(1,i):#(1,3)
+        #print(chr(j+64),chr(63+i),end='\t')
+        print(j,i,end='\t')
+    print()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
