72f37a34414b6c698db65feea6230b3d468b9088
Pradyumna Kulkarni <92165960+pradyumnakulkarni87@users.noreply.github.com>
2021-10-25 21:59:21 +0530

    contains use of for loop

---
+++ forloopusecase.py
@@ -0,0 +1,90 @@
+'''
+#s = [['Abhi',23234],['Sham',534335],['Asha',34364]]
+# list of list
+s= 'Amol hi how r u? please tell me and contact on thisnumber: 9 803 94 80 270'
+s1 = str()
+for i in s.split():
+	if i.isnumeric()==True:
+		s1+=i
+print(s1)
+-------------------------------------------
+import sys
+for i in range(4):
+    #num= int(input('Enter no:.'))
+    print(i)
+else:
+    print('Loop executed...')
+    #sys.exit()
+print('Lets do the new code')
+-----------------------------------------
+# for loop is widely used for Pattern printing
+*
+**
+***
+****
+*****
+
+for i in range(1,6):
+    print(i*'*')
+------------------------
+*
+*   *
+*   *   *
+*   *   *   *
+*   *   *   *   *
+'''
+for i in range(1,6):
+    print(i*'*\t')
+    #print(i*' ')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
