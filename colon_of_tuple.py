64f352597a5d13f90abc29f5cabd00ebefc66670
Pradyumna Kulkarni <92165960+pradyumnakulkarni87@users.noreply.github.com>
2021-11-01 14:00:23 +0530

    contains creation of tuple through deep copy

---
+++ colon_of_tuple.py
@@ -0,0 +1,18 @@
+# Write a Python program to create the colon of a tuple.
+t1 = (10, "python", [20], set())
+t2 = t1
+print(t1)
+t2[2].append(50)
+t2[3].add(40)
+print(t2)
+print(t1)
+
+
+
+
+
+
+
+
+
+
