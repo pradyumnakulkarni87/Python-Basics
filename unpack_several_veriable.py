4257913641c8cd29788c40799072fe05a5386a92
Pradyumna Kulkarni <92165960+pradyumnakulkarni87@users.noreply.github.com>
2021-11-07 23:17:07 +0530

    unpacking of variables through for loop

---
+++ unpack_several_veriable.py
@@ -0,0 +1,4 @@
+# Write a Python program to unpack a tuple in several variables
+x, y, z, t = (10, 20, 30, 40)
+print(x, y, z, t)
+# the number of variables must be equal to the number of items of the tuple
