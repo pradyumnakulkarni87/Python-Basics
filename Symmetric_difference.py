ac05796fa7a492ee85088cb3cd65b587e263f03c
Pradyumna Kulkarni <92165960+pradyumnakulkarni87@users.noreply.github.com>
2021-11-08 23:23:35 +0530

    Py code for subtraction between 2 sets

---
+++ Symmetric_difference.py
@@ -0,0 +1,4 @@
+# Write a Python program to create a symmetric difference.
+s1 = {10, 20, 30, 40, 50, 13, 91}
+s2 = {10, 20, 30, 40, 55, 11, 12}
+print(s1.symmetric_difference(s2))  # its returns set contain uncommon item of both set
