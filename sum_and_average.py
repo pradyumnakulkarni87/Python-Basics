26dff23b24ede98783ce8f396b320ddc51461781
Pradyumna Kulkarni <92165960+pradyumnakulkarni87@users.noreply.github.com>
2021-11-14 00:28:42 +0530

    code for findind sum and average

---
+++ sum_and_average.py
@@ -0,0 +1,19 @@
+# Python Program to find sum and average of List in Python
+l1 = [10, 20, 54, 45, 74, 65, 20, 41]
+s = sum(l1)
+avg = s / len(l1)
+print(f"the sum of list is {s} and average of list is {avg}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
